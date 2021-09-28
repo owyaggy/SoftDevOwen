4cf85992efe552dce101ce8375e6c654f3d21b10
Owen Yaggy <owyaggy@icloud.com>
2021-09-28 09:23:52 -0400

    Fixed a typo in a comment

--- 07_py-csv/07_py-csv.py
+++ 07_py-csv/07_py-csv.py
@@ -4,13 +4,30 @@ import random
 
 def open_csv(fname):
     data = {}
+    sum = 0
     with open(fname, newline='') as f:
         f.readline()
         reader = csv.reader(f, delimiter=',')
         for row in reader:
             if row[0] not in 'Total':
-                data[float(row[1])] = row[0]
+                data[row[0]] = float(row[1])*10
     return data
 
 
-print(open_csv('occupations.csv'))
+def consecutive_sum_dict(data):
+    n = 0
+    for key in data:
+        data[key] += n
+        n = data[key]
+    return data
+
+
+
+
+def main():
+    data = open_csv('occupations.csv')
+    data = consecutive_sum_dict(data)
+    print(data)
+
+
+main()
