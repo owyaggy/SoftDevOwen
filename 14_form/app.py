c23cc4a1e0c129dd20148808d61fa4e25342d522
Owen Yaggy <owyaggy@icloud.com>
2021-10-20 08:26:35 -0400

    15

--- 14_form/app.py
+++ 14_form/app.py
@@ -4,35 +4,54 @@
 # K13
 # 10-14-2021
 
+from flask import Flask  # Facilitate flask webserving
+from flask import render_template  # Facilitate jinja templating
+from flask import request  # Facilitate form submission
 
-from flask import Flask, render_template, request
-app = Flask(__name__) #create instance of class Flask
+# Create Flask object
+app = Flask(__name__)
 
-@app.route("/")       #assign fxn to route
-def hello_world():
-    return render_template('login.html')
 
-@app.route("/", methods=['GET']) 
+"""
+trioTASK:
+~~~~~~~~~~~ BEFORE RUNNING THIS, ~~~~~~~~~~~~~~~~~~
+...read for understanding all of the code below.
+Some will work as written; other sections will not. Can you predict which?
+Devise some simple tests you can run to "take apart this engine," as it were.
+Execute your tests. Process results.
+PROTIP: Insert your own in-line comments wherever they will help your future
+self and/or current teammates understand what is going on.
+"""
+
+
+@app.route("/", methods=["GET"])
 def disp_loginpage():
-    
-    print(request.args)
-    
-  
-    return render_template( 'login.html' )
+    """Returns the login page."""
+    return render_template("login.html")
 
 
-@app.route("/auth", methods=['POST']) # , methods=['GET', 'POST'])
+@app.route("/auth", methods=["POST", "GET"])
 def authenticate():
-    print(request.form)
-    return render_template('response.html', username=request.form['username'])  #response to a form submission
+    """Returns the response page, including the username, request method, and
+    greeting."""
+    if app.debug:
+        print("\n\n\n")
+        print("***DIAG: this Flask obj ***")
+        print(app)
+        print("***DIAG: request obj ***")
+        print(request)
+        print("***DIAG: request.args ***")
+        print(request.args)
+    return render_template(
+        "response.html", username=request.args["username"], method=request.method
+    )
 
+@app.route("/auth", methods=["GET"])
+def provide_user():
+    """Returns the login page"""
+    return render_template("login.html")
 
-    
-if __name__ == "__main__": #false if this file imported as module
-    #enable debugging, auto-restarting of server when this file is modified
-    app.debug = True 
+if __name__ == "__main__":  # False if this file imported as module
+    # Enable debugging, auto-restarting of server when this file is modified
+    app.debug = True
     app.run()
-
-
-app.debug = True
-app.run()
