a8a999f258a323c42a4db6021177f2a162fb4715
Owen Yaggy <owyaggy@icloud.com>
2021-10-24 23:10:31 -0400

    Python to create db from csv, added comments

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -17,25 +17,25 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 
 
 # < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
-try:
+try: # attempt this code (will only work if the database already exists), which resets tables
     c.execute("DROP TABLE courses")
     c.execute("DROP TABLE students")
 except:
-    print("no preexisting database tables found")
+    print("no preexisting database tables found") # if the database doesn't exist
 
 with open('courses.csv') as csvfile:
-    c.execute("CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER)")
-    reader = csv.DictReader(csvfile)
-    for row in reader:
+    c.execute("CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER)") # create courses table
+    reader = csv.DictReader(csvfile) # create reader to process csv as dictionary
+    for row in reader: # iterate through csv rows
         #print(row)
-        c.execute(f"INSERT INTO courses VALUES ('{row['code']}', {row['mark']}, {row['id']})")
+        c.execute(f"INSERT INTO courses VALUES ('{row['code']}', {row['mark']}, {row['id']})") # insert data from that row
 
 with open('students.csv') as csvfile:
-    c.execute("CREATE TABLE students (code TEXT, mark INTEGER, id INTEGER)")
-    reader = csv.DictReader(csvfile)
-    for row in reader:
+    c.execute("CREATE TABLE students (code TEXT, mark INTEGER, id INTEGER)") # create courses table
+    reader = csv.DictReader(csvfile) # create reader to process csv as dictionary
+    for row in reader: # iterate through csv rows
         #print(row)
-        c.execute(f"INSERT INTO students VALUES ('{row['name']}', {row['age']}, {row['id']})")
+        c.execute(f"INSERT INTO students VALUES ('{row['name']}', {row['age']}, {row['id']})") # insert data from that row
 
 '''
 Output Verification:
